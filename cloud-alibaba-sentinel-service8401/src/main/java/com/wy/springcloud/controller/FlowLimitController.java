821c3863a2ee0b0b95b32840d575e1f4dbc2e9dd
王渊 <543456229@qq.com>
2022-12-13 20:05:16 +0800

    sentinel搭建，样例演示

---
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/wy/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,28 @@
+package com.wy.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        // 测试阈值类型：线程数
+        /*try {
+            TimeUnit.MILLISECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
+        return "------testB";
+    }
+
+}
