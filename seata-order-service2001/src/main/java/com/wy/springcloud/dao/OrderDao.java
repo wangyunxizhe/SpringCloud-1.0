300bbcb40cf788b7087b4485a72b9e447406f309
王渊 <543456229@qq.com>
2022-12-16 19:47:16 +0800

    Seata演示之Order模块搭建

---
+++ seata-order-service2001/src/main/java/com/wy/springcloud/dao/OrderDao.java
@@ -0,0 +1,12 @@
+package com.wy.springcloud.dao;
+
+import com.wy.springcloud.domain.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface OrderDao {
+
+
+
+}
